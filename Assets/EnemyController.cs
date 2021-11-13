5f1ee6fec2039cb516e99e3daff8e04ea80f8799
isbr3461 <isbr3461@colorado.edu>
2021-11-13 15:42:54 -0700

    Update EnemyController.cs

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
