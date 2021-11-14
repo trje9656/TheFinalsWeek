e4a9bf9bcaca77e5a970e111083734cdbca8a9d1
isbr3461 <isbr3461@colorado.edu>
2021-11-13 18:02:18 -0700

    monster following

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -13,13 +13,18 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
     }
 
     void OnDrawGizmosSelected()
