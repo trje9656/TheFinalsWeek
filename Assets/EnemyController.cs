1e4f2a0d05ec62c70ed7e82788711247883328cb
isbr3461 <isbr3461@colorado.edu>
2021-11-18 23:43:05 -0700

    target facing during chase

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -25,10 +25,19 @@ public class EnemyController : MonoBehaviour
         if (fov.getSeePlayer())
         {
             agent.SetDestination(target.position);
+            FaceTarget();
         }
         else
         {
             agent.SetDestination(transform.position);
         }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
     }
 }
