6c4ec091430dfac8a52cff9ae8647f5ee6aaa20b
trevjed <trevjed@gmail.com>
2021-12-07 17:50:20 -0700

    flashlight fixes and start of key pickup

--- Assets/FlashLightMech.cs
+++ Assets/FlashLightMech.cs
@@ -7,6 +7,7 @@ public class FlashLightMech : MonoBehaviour
     bool isOn = false;
     public GameObject lightSource;
     public bool failSafe = false;
+    public AudioSource click;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,7 @@ public class FlashLightMech : MonoBehaviour
             {
                 failSafe = true;
                 lightSource.SetActive(true);
+                click.Play();
                 isOn = true;
                 StartCoroutine(FailSafe());
             }
@@ -29,6 +31,7 @@ public class FlashLightMech : MonoBehaviour
             {
                 failSafe = true;
                 lightSource.SetActive(false);
+                click.Play();
                 isOn = false;
                 StartCoroutine(FailSafe());
             }
