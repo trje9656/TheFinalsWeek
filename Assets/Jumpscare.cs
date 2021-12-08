eb0aae0a9e226ba7316af04e7b63ab97eba3b24f
isbr3461 <isbr3461@colorado.edu>
2021-12-08 00:54:56 -0700

    crawler jumpscare

---
+++ Assets/Jumpscare.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscare : MonoBehaviour
+{
+
+    public GameObject jumpscareObject;
+
+    void Start()
+    {
+
+        jumpscareObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.tag == "Player")
+        {
+            jumpscareObject.SetActive(true);
+            StartCoroutine(DestroyObject());
+        }
+    }
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(jumpscareObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/movingcrawler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingcrawler : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
