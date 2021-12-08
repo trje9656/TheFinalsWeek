cd992bb0b1fb5e1f2e716cd90d1d1a2cf9543adb
trevjed <trevjed@gmail.com>
2021-12-07 21:05:41 -0700

    fixed scoring and placed the key locations

--- Assets/ObjectPickup.cs
+++ Assets/ObjectPickup.cs
@@ -5,16 +5,13 @@ using UnityEngine.UI;
 
 public class ObjectPickup : MonoBehaviour
 {
-    public int keys;
-    public GameObject keyText;
+   
     // Start is called before the first frame update
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            keys += 1;
-            keyText.GetComponent<Text>().text = "Keys: " + keys + "/4";
-
+            ScoreUpdate.keys += 1;
             Destroy(gameObject);
         }
             
---
+++ Assets/ScoreUpdate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour
+{
+    public static int keys;
+    public GameObject keyText;
+    // Start is called before the first frame update
+    void Update()
+    {
+           
+            keyText.GetComponent<Text>().text = "Keys: " + keys + "/4";
+
+            
+            
+    }
+}
