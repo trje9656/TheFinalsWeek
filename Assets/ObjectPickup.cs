676cbd544d4b176b67ae641f7304cc1427403fb1
trevjed <trevjed@gmail.com>
2021-12-07 20:42:06 -0700

    (no branch): Auto stash before merge of "HEAD" and "origin/main"

--- Assets/ObjectPickup.cs
+++ Assets/ObjectPickup.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectPickup : MonoBehaviour
 {
+    public int keys;
+    public GameObject keyText;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
+            keys += 1;
+            keyText.GetComponent<Text>().text = "Keys: " + keys + "/4";
 
             Destroy(gameObject);
         }
